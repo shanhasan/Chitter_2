524c0f8f2e54b85cac2ce07c22b34b3eb3d2b260
Shan Hasan <shan119@hotmail.co.uk>
2015-01-05 16:20:11 +0000

    added password validations methods

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -8,12 +8,18 @@ feature "User signs up" do
     expect(User.first.email).to eq("shan119@hotmail.co.uk")
   end
 
+  scenario "with a password that doesn't match" do
+    expect{ sign_up('a@a.com', 'pass', 'wrong')}.to change(User, :count).by(0)
+  end
+
   def sign_up(email = "shan119@hotmail.co.uk",
-              password = "hishan")
+              password = "hishan",
+              password_confirmation = "hishan")
     visit '/users/new'
     expect(page.status_code).to eq(200)
     fill_in :email, :with => email
     fill_in :password, :with => password
+    fill_in :password_confirmation, :with => password_confirmation
     click_button "Sign up"
   end
 end
